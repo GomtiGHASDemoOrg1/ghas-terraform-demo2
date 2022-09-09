0cf88e06a2dce2f46c0571c8e1faf737f2af16e1
Chad Bentz <1760475+felickz@users.noreply.github.com>
2022-09-09 09:34:34 -0400

    Update gcs.tf

--- terraform/gcp/gcs.tf
+++ terraform/gcp/gcs.tf
@@ -2,7 +2,10 @@ resource "google_storage_bucket" "terragoat_website" {
   name          = "terragot-${var.environment}"
   force_destroy = true
 }
-
+# Moving this around to test dismissing Code Scanning alert https://github.com/octodemo/felickz-advanced-security-terraform/security/code-scanning/432
+# Moving this around to test dismissing Code Scanning alert https://github.com/octodemo/felickz-advanced-security-terraform/security/code-scanning/432
+# Moving this around to test dismissing Code Scanning alert https://github.com/octodemo/felickz-advanced-security-terraform/security/code-scanning/432
+# Moving this around to test dismissing Code Scanning alert https://github.com/octodemo/felickz-advanced-security-terraform/security/code-scanning/432
 resource "google_storage_bucket_iam_binding" "allow_public_read" {
   bucket  = google_storage_bucket.terragoat_website.id
   members = ["allUsers"]
