075f0d76aa8bc68d08138496d2d62148567da949
Chad Bentz <1760475+felickz@users.noreply.github.com>
2022-09-09 09:48:54 -0400

    add space

--- terraform/gcp/gcs.tf
+++ terraform/gcp/gcs.tf
@@ -8,6 +8,6 @@ resource "google_storage_bucket" "terragoat_website" {
 # Moving this around to test dismissing Code Scanning alert https://github.com/octodemo/felickz-advanced-security-terraform/security/code-scanning/432
 resource "google_storage_bucket_iam_binding" "allow_public_read" {
   bucket  = google_storage_bucket.terragoat_website.id
-  members = ["allUsers"]
+  members =  ["allUsers"]
   role    = "roles/storage.objectViewer"
 }
